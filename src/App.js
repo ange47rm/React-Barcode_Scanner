d651d3934aae9bb0e2e043b6e99189e66faf6161
Angelo Di Massimo <ange47rm@hotmail.com>
2022-04-12 23:05:40 +0100

    Added logo, time to arrange layout of components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import BarcodeScannerContainer from './containers/BarcodeScannerContainer'
+import BarcodeScannerContainer from './containers/BarcodeScannerContainer';
+import HeaderComponent from './components/HeaderComponent';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <HeaderComponent/>
       <BarcodeScannerContainer/>
     </div>
   );
