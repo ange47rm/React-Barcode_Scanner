43ff9ca0fcf0d341f595171157dbb08d4239ceb1
Angelo Di Massimo <ange47rm@hotmail.com>
2022-04-12 22:10:40 +0100

    Resolved console errors.

--- src/components/OrderComponent.jsx
+++ src/components/OrderComponent.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import OrderLineComponent from './OrderLineComponent';
 
 const OrderComponent = ({ orderNo, orderLines }) => {
 
     const orderLinesNodes = orderLines.map(orderLine => {
         return (
-            <>
-                <OrderLineComponent key={orderLine.objId} scanned={orderLine.scanned} partNo={orderLine.partNo} description={orderLine.description} price={orderLine.price}/>
-            </>
+                <OrderLineComponent scanned={orderLine.scanned} partNo={orderLine.partNo} description={orderLine.description} price={orderLine.price} key={orderLine.objId}/>
         )
     })
+
+    useEffect(() => {
+        console.log(orderNo)
+        console.log(orderLines)
+    }, [])
 
     return (
         <>
